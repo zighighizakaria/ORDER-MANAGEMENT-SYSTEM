03455d26fbd71c382dda5301a7f7ab89299d0970
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-12-23 16:45:27 +0100

    consul config gateway customer

---
+++ customer-service/src/main/java/org/sid/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,36 @@
+package org.sid.customerservice;
+
+import org.sid.customerservice.entities.Customer;
+import org.sid.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CustomerRepository customerRepository){
+		 return args -> {
+
+			 customerRepository.saveAll(List.of(
+					 Customer.builder().name("simo").email("simo@gmail.com").build(),
+					 Customer.builder().name("moha").email("moha@gmail.com").build(),
+					 Customer.builder().name("rabii").email("rabii@gmail.com").build()
+			 ));
+			 customerRepository.findAll().forEach(System.out::println);
+		 };
+
+	}
+
+}

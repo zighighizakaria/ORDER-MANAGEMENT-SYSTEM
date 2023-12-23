06bf8335d95ed8bc053dbe7822d82f845022ad38
zighighizakaria <zighighi.zakaria.2000@gmail.com>
2023-12-23 21:09:46 +0100

    order-service done

---
+++ order-service/src/main/java/org/sid/orderservice/repositories/ProductItemRepository.java
@@ -0,0 +1,10 @@
+package org.sid.orderservice.repositories;
+
+import org.sid.orderservice.entities.ProductItem;
+import org.sid.orderservice.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
+}
